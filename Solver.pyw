b984a726d9e7888dea49c347fb05cf103b723a12
Khushiyant <khushiyant@learner.com>
2020-08-14 16:59:33 +0530

    Commenting the source Code

--- Solver.pyw
+++ Solver.pyw
@@ -1,22 +1,28 @@
+'''Basic Import the basic libraries'''
+
 import logic as lg
 from tkinter import Button,Tk,Entry,LEFT,Frame
 
-inp = []
+inp = []    
 for _ in range(0,9):
-    inp.append([0,0,0,0,0,0,0,0,0])
-grid = inp.copy()
+    inp.append([0,0,0,0,0,0,0,0,0]) ## Creating a null nested list ##
+grid = inp.copy()                   ## Creating a replication of null nested list ##
 
 if __name__ == "__main__":
     my_font = ("arial",18)
     
-    root = Tk()
-    root.resizable(width=False,height=False)
+    root = Tk()     ## Initalisation of GUI ##
+    root.resizable(width=False,height=False)    ## Fixing the GUI geometry ##
     root.title("Sudoku Solver")
+
+    ''' Elements Declaration '''
 
     frame1 = Frame(root)
     frame1.pack(side=LEFT)
     frame2 = Frame(root)
     frame2.pack(side = LEFT)
+
+    '''Code to generate checkboard pattern'''
 
     for i in range(9):
         for j in range(9):
@@ -26,7 +32,12 @@ if __name__ == "__main__":
             else:
                 grid[i][j] = Entry(frame1,bg="white",highlightcolor="yellow",font=my_font,width=2)
                 grid[i][j].grid(row=i,column=j)
-
+    '''
+    This Function perform the following tasks:
+    -> getting the entered values in grid
+    -> passing the fetched values to sovleSudoku()
+    -> assigning the solved grid to GUI
+    '''
     def access():
         for a in range(9):
             for b in range(9):
@@ -43,4 +54,4 @@ if __name__ == "__main__":
     solve = Button(frame2,text="Solve",height=12,fg="blue",font=("arial",15),activebackground="skyblue",command=access)
     solve.pack(side=LEFT)
 
-    root.mainloop()
+    root.mainloop()     ## End of GUI ##
--- logic.py
+++ logic.py
@@ -1,4 +1,4 @@
-def NextCell(grid, i, j):
+def NextCell(grid, i, j):   ## Find the Index of the Grid ##
     for x in range(i,9):
         for y in range(j,9):
             if grid[x][y] == 0:
@@ -9,27 +9,33 @@ def NextCell(grid, i, j):
                 return x,y
     return -1,-1
 
-def isValid(grid, i, j, e):
-    isRowOk = all([e != grid[i][i] for x in range(9)]) 
+def isValid(grid, i, j, e):         ## Find whether the entered sudoku grid is valid to solve ##
+    isRowOk = all([e != grid[i][i] for x in range(9)])      ## Validate the row ##
     if isRowOk:
-        isColumnOk = all([e != grid[x][j] for x in range(9)])
+        isColumnOk = all([e != grid[x][j] for x in range(9)])   ## Validate the Column ##
         if isColumnOk:
-            secTopX, secTopY = 3*(i//3), 3*(j//3)
+            secTopX, secTopY = 3*(i//3), 3*(j//3)              ## Main logic intials ##
             for x in range(secTopX, secTopX+3):
                 for y in range(secTopY, secTopY+3):
                     if grid[x][y] == 0:
                         return False
             return True
     return False
+'''
 
-def solveSudoku(grid, i=0, j=0):
+solveSudoku()
+This Function uses recurion to Solve the Sudoku Grid
+It also acts as calling function
+
+'''
+def solveSudoku(grid, i=0, j=0):        ## Main Calling Function ##
     i,j = NextCell(grid, i, j)
-    if i == -1:
+    if i == -1:     ## If NextCell give indexing of end cell ##
         return True
     for e in range(1,10):
         if isValid(grid, i, j, e):
             grid[i][j] = e
-            if solveSudoku(grid,i,j):
+            if solveSudoku(grid,i,j):   ## Recursion for Solving ##
                 return True
             grid[i][j] = 0
     return False
